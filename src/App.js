f97d837ca489798513cae7cbd7ccda218d80b415
Baji Syed <sdbaji.baji72@gmail.com>
2020-05-12 13:37:58 +0530

    fixed div id's issue

--- src/App.js
+++ src/App.js
@@ -1,34 +1,18 @@
 import React from 'react';
 import './App.css';
-
 import Addition from './Components/Addition/Addition';
 
 const App = props => {
-
   const additionHandler = () => {
-    const num1 = Number(document.querySelector('#Text1').value);
-    const num2 = Number(document.querySelector('#Text2').value);
+    const num1 = Number(document.querySelector('#txt1').value);
+    const num2 = Number(document.querySelector('#txt2').value);
     const sum = num1 + num2;
-    document.querySelector('#txtresult').value = sum;
+    document.querySelector('#result').value = sum;
   }
-
-  const dataChangeHandler = () => {
-   if(Number(document.querySelector('#Text1').value) !== 0 || Number(document.querySelector('#Text2').value) !== 0){
-    document.querySelector('#clickbtn').disabled = false;
-   } else {
-    document.querySelector('#clickbtn').disabled = true;
-   }
-  }
-
-
   return (
     <div className="App">
-      <Addition 
-        addMethod={additionHandler} 
-        dataChanged={dataChangeHandler}
-        />
+      <Addition addMethod={additionHandler} />
     </div>
   );
 }
-
 export default App;
--- src/Components/Addition/Addition.js
+++ src/Components/Addition/Addition.js
@@ -1,28 +1,35 @@
 import React from 'react';
-
 import './Addition.css'
 
 const Addition = (props) => {
+    const dataChangeHandler = () => {
+        if(document.querySelector('#txt1').value !== '' && document.querySelector('#txt2').value !== ''){
+         document.querySelector('input[name="clickbutton"]').disabled = false;
+        } else {
+         document.querySelector('input[name="clickbutton"]').disabled = true;
+        }
+       }
+
     return (
         <div className="Container Vertical-Center">
-            <div>
+            <div id="Text1">
                 <label>Enter First Number : </label>
-                <input type="number" id="Text1" onChange={props.dataChanged} />
+                <input type="number" id="txt1" onChange={dataChangeHandler} />
             </div>
-            <div>
+            <div id="Text2">
                 <label>Enter Second Number : </label>
-                <input type="number" id="Text2" onChange={props.dataChanged} /><br/>
+                <input type="number" id="txt2" onChange={dataChangeHandler} /><br/>
             </div>
             <br/>
-            <div>
+            <div id="txtresult">
                 <label>Result : </label>
-                <input type="number" id="txtresult"/><br/>
+                <input type="number" id="result" /><br/>
             </div>
             <br/>
-            <div>
-                <input type="button" value="Display Result" id="clickbtn" onClick={props.addMethod} />
+            <div name="clickbtn">
+                <input type="button" value="Display Result" name="clickbutton" onClick={props.addMethod}/>
             </div>
-        </div> 
+        </div>
     );
 }
 
